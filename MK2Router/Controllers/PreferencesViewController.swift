3ea45efd4a7273a307ffd2546dcd3b724ae35203
imk2o <imk2o@icloud.com>
2016-09-04 13:54:37 +0900

    #3 performコンビニエンスメソッドを削除、サンプルコード修正

---
+++ MK2Router/Controllers/PreferencesViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PreferencesViewController.swift
+//  MK2Router
+//
+//  Created by k2o on 2016/09/04.
+//  Copyright © 2016年 Yuichi Kobayashi. All rights reserved.
+//
+
+import UIKit
+import MK2Router
+
+class PreferencesViewController: UITableViewController {
+
+    @IBOutlet weak var versionCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let version = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String {
+            self.versionCell.detailTextLabel?.text = version
+        }
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func dismiss(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+extension PreferencesViewController: MK2Router.DestinationType {
+    typealias Context = Void
+}
